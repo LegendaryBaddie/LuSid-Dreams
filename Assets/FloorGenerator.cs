d1c1d0f9385f60c49edaf478c6f73fcb8ce4d92d
Austin White <arw.austin@gmail.com>
2016-02-12 10:51:03 -0500

    Some work on floor/room generation

---
+++ Assets/FloorGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorGenerator : MonoBehaviour {
+
+    public GameObject plane;
+
+    // Use this for initialization
+    void Start () {
+    //Load Tilesets& such
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	// Choose a tile set
+
+    //Generate a floor with that tileset
+
+	}
+}
--- Assets/RoomGenerate.cs
+++ Assets/RoomGenerate.cs
@@ -4,25 +4,34 @@ using System.Collections;
 public class RoomGenerate : MonoBehaviour {
     public GameObject basicTile;
     public GameObject plane;
+   
     int[,] roomMaxtrix =new int[10, 10];
     // Use this for initialization
     void Start () {
-     int xArray = roomMaxtrix.GetLength(0);
-     int yArray = roomMaxtrix.GetLength(1);
     
-    for (int i=0; i<xArray;i++)
-        {
-            for (int m = 0; m < yArray; m++)
-            {
-                Instantiate(basicTile,new Vector3(i , m,0), Quaternion.identity);
-                
-            }
-        }
-
     }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    public void Generate()
+    {
+        Instantiate(plane, new Vector3(0, 0, 0), Quaternion.identity);
+        int xArray = roomMaxtrix.GetLength(0);
+        int yArray = roomMaxtrix.GetLength(1);
+
+        for (int i = 0; i < xArray; i++)
+        {
+            for (int m = 0; m < yArray; m++)
+            {
+                // instantate a new tile as child with a unique name
+                GameObject tile = (GameObject)Instantiate(basicTile, new Vector3(i, m, 0), Quaternion.identity);
+                tile.transform.parent = GameObject.Find("Plane(Clone)").transform;
+                // 0,0 is bottome left hand corner.
+                tile.name = "Tile Column:" + i + "Row:" + m;
+            }
+        }
+
+    }
 }
