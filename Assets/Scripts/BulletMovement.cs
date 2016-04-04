5cb95d5eb84e7d0bb39f20158cdfd95a29af6d20
JARED SIMONELLI (RIT Student) <jas1421@ad.rit.edu>
2016-04-04 10:57:26 -0400

    added a whole bunch of stuff lolol

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMovement : MonoBehaviour {
+
+	GameObject player;
+	//GameObject[] Enemys;
+	int speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		speed = 5;
+		player =  GameObject.FindGameObjectWithTag("ProjStart");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position += player.transform.right * Time.deltaTime * speed;
+	}
+
+	//Check Collision with enemy
+	void onTriggerEnter(Collider col){
+		print ("Here");
+
+		//Deal damage to enemy
+		if (col.gameObject.tag == "Enemy") 
+			col.gameObject.GetComponent<EnemyAttributes>().takeDamage(10);
+
+	}
+}
